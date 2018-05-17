7622028031fb7e82399ac7ce0bf3b7bf92ff5bae
Ruaidhri Devery <deveryr2@gmail.com>
2018-05-16 20:52:53 -0700

    Support status in treetop.Writer utility

--- writer.go
+++ writer.go
@@ -1,21 +1,33 @@
 package treetop
 
 import (
-	"io"
 	"net/http"
 	"strings"
 )
 
+type PartialWriter interface {
+	Write([]byte) (int, error)
+	Status(int)
+}
+
 type treetopWriter struct {
 	responseWriter http.ResponseWriter
+	status         int
+}
+
+func (t *treetopWriter) Status(code int) {
+	t.status = code
 }
 
 func (tw *treetopWriter) Write(p []byte) (n int, err error) {
 	tw.responseWriter.Header().Set("Content-Type", FragmentContentType)
+	if tw.status > 100 {
+		tw.responseWriter.WriteHeader(tw.status)
+	}
 	return tw.responseWriter.Write(p)
 }
 
-func Writer(w http.ResponseWriter, req *http.Request) (io.Writer, bool) {
+func Writer(w http.ResponseWriter, req *http.Request) (PartialWriter, bool) {
 	var isPartial bool
 	for _, accept := range strings.Split(req.Header.Get("Accept"), ",") {
 		if strings.TrimSpace(accept) == FragmentContentType {
@@ -26,5 +38,5 @@ func Writer(w http.ResponseWriter, req *http.Request) (io.Writer, bool) {
 	if !isPartial {
 		return nil, false
 	}
-	return &treetopWriter{w}, true
+	return &treetopWriter{w, 200}, true
 }
