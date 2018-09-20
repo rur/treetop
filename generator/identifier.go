245134e234e2578ef776b88816bd192a7bbc1cab
Ruaidhri Devery <deveryr2@gmail.com>
2018-09-20 10:18:51 -0700

    Most of identifiers module is not being used any more

--- generator/identifier.go
+++ generator/identifier.go
@@ -1,31 +1,9 @@
 package generator
 
 import (
-	"fmt"
-	"log"
-	"regexp"
 	"strings"
 	"unicode"
 )
-
-type uniqueIdentifiers struct {
-	ref chan map[string]bool
-}
-
-func newIdentifiers() uniqueIdentifiers {
-	idents := uniqueIdentifiers{make(chan map[string]bool, 1)}
-	idents.ref <- make(map[string]bool)
-	return idents
-}
-
-// pretty lo-fi slugify, remove all non-alphanum and lowercase first character
-func snakify(name string) string {
-	reg, err := regexp.Compile("[^a-zA-Z0-9-_]+")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return strings.ToLower(reg.ReplaceAllString(name, "-"))
-}
 
 func delims(r rune) bool {
 	return !unicode.IsLetter(r) && !unicode.IsNumber(r)
@@ -71,68 +49,4 @@ func ValidPublicIdentifier(name string) string {
 		fixed[i] = string(mutable)
 	}
 	return strings.Join(fixed, "")
-}
-
-// create a new identifier that is unique relative to u instance
-func (u *uniqueIdentifiers) new(name string, qualifier []string) string {
-	var found bool
-	// this is a statefull method so I'm using a channel as a locking mechanism
-	ref := <-u.ref
-	defer func() {
-		u.ref <- ref
-	}()
-
-	ident := ValidIdentifier(name)
-
-	if len(qualifier) > 0 {
-		ident = ValidIdentifier(strings.Join(qualifier, " ")) + "_" + strings.Title(ident)
-	}
-
-	if _, found = ref[ident]; !found {
-		ref[ident] = true
-		return ident
-	}
-	i := 1
-	var identI string
-	for {
-		identI = fmt.Sprintf("%s%v", ident, i)
-		if _, found = ref[identI]; !found {
-			ref[identI] = true
-			return identI
-		}
-		i += 1
-	}
-}
-
-func (u *uniqueIdentifiers) copy() *uniqueIdentifiers {
-	ref := <-u.ref
-	defer func() {
-		u.ref <- ref
-	}()
-	newIdent := newIdentifiers()
-	newRef := <-newIdent.ref
-	for k, v := range ref {
-		newRef[k] = v
-	}
-	newIdent.ref <- newRef
-	return &newIdent
-}
-
-func (u *uniqueIdentifiers) reserve(ident string) string {
-	ref := <-u.ref
-	defer func() {
-		u.ref <- ref
-	}()
-	ref[ident] = true
-	return ident
-}
-
-func (u *uniqueIdentifiers) exists(ident string) bool {
-	// this is a statefull method so I'm using a channel as a locking mechanism
-	ref := <-u.ref
-	defer func() {
-		u.ref <- ref
-	}()
-	_, found := ref[ident]
-	return found
 }
--- generator/identifier_test.go
+++ generator/identifier_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_validIdentifier(t *testing.T) {
+func Test_ValidIdentifier(t *testing.T) {
 	type args struct {
 		name string
 	}
@@ -23,49 +23,8 @@ func Test_validIdentifier(t *testing.T) {
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := validIdentifier(tt.args.name); got != tt.want {
-				t.Errorf("validIdentifier() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_uniqueIdentifiers_new(t *testing.T) {
-	ref := make(chan map[string]bool, 1)
-	ref <- map[string]bool{
-		"test": true,
-	}
-
-	type fields struct {
-		ref chan map[string]bool
-	}
-	type args struct {
-		name      string
-		qualifier string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   string
-	}{{
-		"basic test",
-		fields{ref: ref},
-		args{"test", "Handler"},
-		"testHandler",
-	}, {
-		"snakecase test",
-		fields{ref: ref},
-		args{"testIsInTheHeart!!", "Handler"},
-		"testIsInTheHeart",
-	}}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			u := &uniqueIdentifiers{
-				ref: tt.fields.ref,
-			}
-			if got := u.new(tt.args.name, tt.args.qualifier); got != tt.want {
-				t.Errorf("uniqueIdentifiers.new() = %v, want %v", got, tt.want)
+			if got := ValidIdentifier(tt.args.name); got != tt.want {
+				t.Errorf("ValidIdentifier() = %v, want %v", got, tt.want)
 			}
 		})
 	}
