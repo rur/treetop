b9abf9c1f3812e74dca8768fcf19f25006bf37fd
Ruaidhri Devery <deveryr2@gmail.com>
2020-02-22 23:47:17 -0800

    Collect handler helpers into one file

--- helpers.go
+++ helpers.go
@@ -44,3 +44,21 @@ func IsTreetopRequest(req *http.Request) bool {
 	}
 	return false
 }
+
+// Redirect is a helper that will instruct the Treetop client library to direct the web browser
+// to a new URL. If the request is not from a Treetop client, the 3xx redirect method is used.
+//
+// This is necessary because 3xx HTTP redirects are opaque to XHR, when a full browser redirect
+// is needed a 'X-Treetop-Redirect' header is used.
+//
+// Example:
+// 		treetop.Redirect(w, req, "/some/other/path", http.StatusSeeOther)
+//
+func Redirect(w http.ResponseWriter, req *http.Request, location string, status int) {
+	if IsTreetopRequest(req) {
+		w.Header().Add("X-Treetop-Redirect", "SeeOther")
+		http.Redirect(w, req, location, 200) // must be 200 because XHR cannot intercept a 3xx redirect
+	} else {
+		http.Redirect(w, req, location, status)
+	}
+}
--- helpers_test.go
+++ helpers_test.go
@@ -6,6 +6,37 @@ import (
 	"testing"
 )
 
+func TestIsTreetopRequest(t *testing.T) {
+	tests := []struct {
+		name string
+		req  *http.Request
+		want bool
+	}{
+		{
+			name: "basic any content type",
+			req:  mockRequest("/Some/path", "*/*"),
+			want: false,
+		},
+		{
+			name: "single specific contnet type",
+			req:  mockRequest("/Some/path", TemplateContentType),
+			want: true,
+		},
+		{
+			name: "basic any content type",
+			req:  mockRequest("/Some/path", "text/html; "+TemplateContentType),
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsTreetopRequest(tt.req); got != tt.want {
+				t.Errorf("IsTreetopRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestRedirect(t *testing.T) {
 	type args struct {
 		w        *httptest.ResponseRecorder
