1416c30f810043a513c0d34e5e8182609c27d211
Ruaidhri Devery <deveryr2@gmail.com>
2020-03-01 21:20:01 -0800

    Add FileSystemExecutor implementation

--- execFileSystem.go
+++ execFileSystem.go
@@ -5,66 +5,82 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"path/filepath"
+	"text/template/parse"
 )
 
 // FileSystemExecutor loads view templates as a path from a Go HTML template file.
 // The underlying file system is abstracted through the http.FileSystem interface to allow for
 // in-memory use.
 type FileSystemExecutor struct {
-	FS http.FileSystem
+	FS   http.FileSystem
+	exec Executor
 }
 
-// NewViewHandler will create a Handler instance capable of serving treetop requests
-// for the supplied view configuration
-//
-// TODO: Implement this
-func (ft *FileSystemExecutor) NewViewHandler(view *View, includes ...*View) ViewHandler {
-	page, part, incls := CompileViews(view, includes...)
-	handler := &TemplateHandler{
-		Page:            page,
-		PageTemplate:    ft.MustParseTemplateFiles(page),
-		Partial:         part,
-		PartialTemplate: ft.MustParseTemplateFiles(part),
-		Includes:        incls,
-	}
-	for _, inc := range incls {
-		handler.IncludeTemplates = append(handler.IncludeTemplates, ft.MustParseTemplateFiles(inc))
-	}
-	return handler
+// NewViewHandler creates a ViewHandler from a View endpoint definition treating
+// view template strings as keys into the string template dictionary.
+func (fe *FileSystemExecutor) NewViewHandler(view *View, includes ...*View) ViewHandler {
+	fe.exec.NewTemplate = fe.constructTemplate
+	return fe.exec.NewViewHandler(view, includes...)
 }
 
-// MustParseTemplateFiles will load template files and parse contents into a HTML template instance
-// it will use the supplied http.FileSystem for loading the template files
-//
-// TODO: Implement this
-func (ft *FileSystemExecutor) MustParseTemplateFiles(view *View) *template.Template {
-	var t *template.Template
-	// snippet based upon https://golang.org/pkg/html/template/#ParseFiles implementation
-	for _, filename := range GetTemplateList(view) {
-		buffer := new(bytes.Buffer)
-		file, err := ft.FS.Open(filename)
-		if err != nil {
-			panic(fmt.Sprintf("Failed to open template file '%s', error %s", filename, err.Error()))
-		}
-		_, err = buffer.ReadFrom(file)
-		if err != nil {
-			panic(fmt.Sprintf("Failed to read contents of template file '%s', error %s", filename, err.Error()))
-		}
-		s := buffer.String()
-		name := filepath.Base(filename)
-		var tmpl *template.Template
-		if t == nil {
-			// first file in the list is used as the root template
-			t = template.New(name)
-			tmpl = t
+// FlushErrors will return a list of all template generation errors that occurred
+// while ViewHandlers were being created by this executor
+func (fe *FileSystemExecutor) FlushErrors() ExecutorErrors {
+	return fe.exec.FlushErrors()
+}
+
+// constructTempalate for FileSystemExecutor will treat the template string of each view
+// as a template in of itself.
+func (fe *FileSystemExecutor) constructTemplate(view *View) (*template.Template, error) {
+	if view == nil {
+		return nil, nil
+	}
+	var out *template.Template
+	cache := make(map[string]*parse.Tree)
+	buffer := new(bytes.Buffer)
+
+	queue := viewQueue{}
+	queue.add(view)
+
+	for !queue.empty() {
+		v, _ := queue.next()
+		var t *template.Template
+		if out == nil {
+			out = template.New(v.Defines)
+			t = out
 		} else {
-			tmpl = t.New(name)
+			t = out.New(v.Defines)
 		}
-		_, err = tmpl.Parse(s)
-		if err != nil {
-			panic(fmt.Sprintf("Error parsing template %s, error %s", filename, err.Error()))
+
+		if tree, ok := cache[v.Template]; ok {
+			t.AddParseTree(v.Defines, tree)
+		} else {
+			buffer.Reset()
+			file, err := fe.FS.Open(v.Template)
+			if err != nil {
+				return nil, fmt.Errorf(
+					"Failed to open template file '%s', error %s",
+					v.Template, err.Error(),
+				)
+			}
+			_, err = buffer.ReadFrom(file)
+			if err != nil {
+				return nil, fmt.Errorf(
+					"Failed to read contents of template file '%s', error %s",
+					v.Template, err.Error(),
+				)
+			}
+			_, err = t.Parse(buffer.String())
+			if err != nil {
+				return nil, fmt.Errorf(
+					"Failed to parse contents of template file '%s', error %s",
+					v.Template, err.Error(),
+				)
+			}
+		}
+		for _, sub := range v.SubViews {
+			queue.add(sub)
 		}
 	}
-	return t
+	return out, nil
 }
---
+++ execFileSystem_test.go
@@ -0,0 +1,155 @@
+package treetop
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFileSystemExecutor_NewViewHandler(t *testing.T) {
+	standardHandler := func(exec ViewExecutor) ViewHandler {
+		base := NewView("base.html", Constant(struct {
+			Content interface{}
+			PS      interface{}
+		}{
+			Content: struct {
+				Message string
+				Sub     interface{}
+			}{
+				Message: "from base to content",
+				Sub:     "from base via content to sub",
+			},
+			PS: "from base to ps",
+		}))
+		content := base.NewSubView("content", "content.html", Constant(struct {
+			Message string
+			Sub     interface{}
+		}{
+			Message: "from content to content!",
+			Sub:     "from content to sub",
+		}))
+		content.NewDefaultSubView("sub", "sub.html", Constant("from sub to sub"))
+		ps := base.NewSubView("ps", "ps.html", Constant("from ps to ps"))
+		return exec.NewViewHandler(content, ps)
+	}
+
+	tests := []struct {
+		name           string
+		getHandler     func(exec ViewExecutor) ViewHandler
+		expectPage     string
+		expectTemplate string
+		expectErrors   []string
+		pageOnly       bool
+		templateOnly   bool
+	}{
+		{
+			name:       "functional example",
+			getHandler: standardHandler,
+			expectPage: stripIndent(`<html><body>
+			<div id="content">
+			<p>Given from base to content</p>
+			<p id="sub">Given from base via content to sub</p>
+			</div>
+			
+			<div id="ps">Given from base to ps</div>
+			</body></html>`),
+			expectTemplate: stripIndent(`<template>
+			<div id="content">
+			<p>Given from content to content!</p>
+			<p id="sub">Given from content to sub</p>
+			</div>
+			<div id="ps">Given from ps to ps</div>
+			</template>`),
+		},
+		{
+			name:       "page only",
+			getHandler: standardHandler,
+			expectPage: stripIndent(`<html><body>
+			<div id="content">
+			<p>Given from base to content</p>
+			<p id="sub">Given from base via content to sub</p>
+			</div>
+			
+			<div id="ps">Given from base to ps</div>
+			</body></html>`),
+			expectTemplate: "Not Acceptable\n",
+			pageOnly:       true,
+		},
+		{
+			name:       "template only",
+			getHandler: standardHandler,
+			expectPage: "Not Acceptable\n",
+			expectTemplate: stripIndent(`<template>
+			<div id="content">
+			<p>Given from content to content!</p>
+			<p id="sub">Given from content to sub</p>
+			</div>
+			<div id="ps">Given from ps to ps</div>
+			</template>`),
+			templateOnly: true,
+		},
+		{
+			name: "file not found",
+			getHandler: func(exec ViewExecutor) ViewHandler {
+				return exec.NewViewHandler(NewView("notexists.html", Noop))
+			},
+			expectPage:     "Not Acceptable\n",
+			expectTemplate: "Not Acceptable\n",
+			expectErrors: []string{
+				`Failed to open template file 'notexists.html', ` +
+					`error open testdata/notexists.html: no such file or directory`,
+				`Failed to open template file 'notexists.html', ` +
+					`error open testdata/notexists.html: no such file or directory`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			exec := &FileSystemExecutor{
+				FS: http.Dir("testdata"),
+			}
+			handler := tt.getHandler(exec)
+			if tt.pageOnly {
+				handler = handler.PageOnly()
+			}
+			if tt.templateOnly {
+				handler = handler.FragmentOnly()
+			}
+			rec := httptest.NewRecorder()
+			handler.ServeHTTP(rec, mockRequest("/some/path", "*/*"))
+			gotPage := stripIndent(sDumpBody(rec))
+
+			if gotPage != tt.expectPage {
+				t.Errorf("Expecting page body\n%s\nGot\n%s", tt.expectPage, gotPage)
+			}
+
+			rec = httptest.NewRecorder()
+			handler.ServeHTTP(rec, mockRequest("/some/path", TemplateContentType))
+			gotTemplate := stripIndent(sDumpBody(rec))
+
+			if gotTemplate != tt.expectTemplate {
+				t.Errorf("Expecting partial body\n%s\nGot\n%s", tt.expectTemplate, gotTemplate)
+			}
+
+			gotErrors := exec.FlushErrors()
+			for len(gotErrors) < len(tt.expectErrors) {
+				gotErrors = append(gotErrors, nil)
+			}
+
+			for i, err := range gotErrors {
+				if err == nil {
+					t.Errorf("Expecting an error [%d]: %s", i, tt.expectErrors[i])
+					continue
+				}
+				if i >= len(tt.expectErrors) {
+					t.Errorf("Unexpected error [%d]: %s", i, err.Error())
+					continue
+				}
+				if got := err.Error(); got != tt.expectErrors[i] {
+					t.Errorf("Expecting error [%d]\n%s\ngot\n%s", i, tt.expectErrors[i], got)
+				}
+			}
+		})
+	}
+}
