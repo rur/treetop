a23d128425321809b1c03348300c5d3fe0d527b4
Ruaidhri Devery <deveryr2@gmail.com>
2021-10-02 23:39:05 -0700

    Update changelog and bolster tests

--- handler_test.go
+++ handler_test.go
@@ -49,6 +49,7 @@ func setupTemplateHandler() ViewHandler {
 	})
 	base.NewDefaultSubView("nav", "nav.html", Noop)
 	content := base.NewSubView("content", "content.html", func(resp Response, req *http.Request) interface{} {
+		resp.Header().Add("Vary", "Cookie")
 		return struct {
 			Message    string
 			SubContent interface{}
@@ -92,6 +93,12 @@ func TestTemplateHandler_PartialRequest(t *testing.T) {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(rec.Body)
 
+	gotVary := strings.Join(rec.Header().Values("Vary"), ", ")
+	expectVary := "Cookie, Accept"
+	if gotVary != expectVary {
+		t.Errorf("Expecting Vary header: [%s], got: [%s]", expectVary, gotVary)
+	}
+
 	if body := buf.String(); body != expecting {
 		t.Errorf("Expecting body \n%s\nGOT\n%s", expecting, body)
 	}
@@ -121,8 +128,7 @@ func TestTemplateHandler_PageRequestNotAcceptable(t *testing.T) {
 }
 
 func TestTemplateHandler_PageRequest(t *testing.T) {
-	th := setupTemplateHandler()
-	handler := th.PageOnly()
+	handler := setupTemplateHandler().PageOnly()
 	rec := httptest.NewRecorder()
 	handler.ServeHTTP(rec, mockRequest("/some/path", "*/*"))
 
@@ -155,6 +161,12 @@ func TestTemplateHandler_PageRequest(t *testing.T) {
 
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(rec.Body)
+
+	gotVary := strings.Join(rec.Header().Values("Vary"), ", ")
+	expectVary := "Cookie"
+	if gotVary != expectVary {
+		t.Errorf("Expecting Vary header: [%s], got: [%s]", expectVary, gotVary)
+	}
 
 	if body := buf.String(); body != expecting {
 		t.Errorf("Expecting body \n%s\ngot\n%s", expecting, body)
